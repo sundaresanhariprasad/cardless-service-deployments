6f74c9a250d477d4a012f39721478a70e58863a9
harideveloper <hariprasad.sundharesan@gmail.com>
2021-10-06 13:51:11 +0100

    Testing CI/CD

---
+++ infra-test/variables.tf
@@ -0,0 +1,52 @@
+variable "project" {
+  description = "The Google Cloud Platform project ID to target."
+  default     = "lbgretailpocs"
+}
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  default     = "cardless-gke-cluster-test"
+}
+
+variable "initial_node_count" {
+  description = "Default amount of nodes per zone that will be launched"
+  default     = 1
+}
+
+variable "primary_region" {
+  description = "The primary region provision resources within."
+  default     = "us-central1"
+}
+
+variable "primary_zone" {
+  description = "The primary zone to provision resources within."
+  default     = "us-central1-a"
+}
+
+
+variable "machine_type" {
+  description = "Machine type for the kubernetes nodes"
+  default     = "custom-10-30720"
+}
+
+variable "preemptible_vm" {
+  description = "Allocate preemptible VMs type"
+  default     = "true"
+}
+
+
+variable "network" {
+  default = "cardless-network"
+}
+
+variable "subnetwork" {
+  default = "cardless-sub-network"
+}
+
+variable "gke-service-account" {
+  default = "cardless-gke-service-account"
+}
+
+variable "gke-service-account-display-name" {
+  default = "cardless-gke-service-account"
+}
