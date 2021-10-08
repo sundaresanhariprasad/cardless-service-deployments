4620b7d5834d77ca2f6e907011f76811867cb931
sundaresanhariprasad <hari.gcp.study@gmail.com>
2021-10-08 13:24:52 +0100

    Setting up the k8 deployment for backend services

--- k8/Cleanup-k8.sh
+++ k8/Cleanup-k8.sh
@@ -1,9 +1,9 @@
 # Start up scripts to all the relevant services (Development Env only)
 echo "Creating components starts" 
 #kubectl create namespace dev-namespace
-kubectl create -f ./cards-legacy/
-kubectl create -f ./cards-new-platform/
-kubectl create -f ./bridge-service/
+kubectl delete -f ./cards-legacy/
+kubectl delete -f ./cards-new-platform/
+kubectl delete -f ./bridge-service/
 echo "Creating components ends" 
 kubectl get services
 kubectl get pods
